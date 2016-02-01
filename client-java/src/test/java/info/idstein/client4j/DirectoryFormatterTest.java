e9f548bb2a5e53e5baf844fcc311d6c2bccdf292
Paul Idstein <paul@idstein.info>
2016-02-01 19:13:37 +0100

    Add java client test for formatter
    Add simple empty directory and nesting test to java client

---
+++ client-java/src/test/java/info/idstein/client4j/DirectoryFormatterTest.java
@@ -0,0 +1,32 @@
+package info.idstein.client4j;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import info.idstein.services.pathvisitor.model.Directory;
+import info.idstein.services.pathvisitor.model.TextFile;
+
+public class DirectoryFormatterTest {
+
+	@Test
+	public void emptyDirectory() {
+		DirectoryFormatter formatter = new DirectoryFormatter(TextFile.class);
+		assertThat(formatter.format(new Directory()), equalTo(""));
+	}
+	
+	@Test
+	public void nestedDirectoryStartWithTab() {
+		final Directory root = new Directory();
+		final Directory nestedA = new Directory("nestedA");
+		final Directory nestedB = new Directory("nestedB");
+		nestedA.addChild(nestedB);
+		root.addChild(nestedA);
+		DirectoryFormatter formatter = new DirectoryFormatter(TextFile.class);
+		assertThat(formatter.format(root), containsString("\tnestedB"));
+	}
+
+}
